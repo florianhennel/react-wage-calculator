b347f8d2c635de02d443247442d03358cfff8abd
florianhennel <hennelflori@gmail.com>
2024-04-29 20:35:38 +0200

    only formatting

--- src/components/HouseholdSalaryCalculator.tsx
+++ src/components/HouseholdSalaryCalculator.tsx
@@ -2,7 +2,7 @@ import { useState, createContext, useEffect } from "react";
 import FamilyMemberTabs from "./FamilyMemberTabs/FamilyMemberTabs";
 import HouseholdSummary from "./HouseholdSummary/HouseholdSummary";
 import SalaryCalculator from "./SalaryCalculator/SalaryCalculator";
-import { Skeleton } from "@/components/ui/skeleton"
+import { Skeleton } from "@/components/ui/skeleton";
 
 export interface familyMember {
     id: number;
@@ -15,67 +15,75 @@ export interface familyMember {
     taxRelief: boolean;
     familyDiscount: boolean;
     dependents: number;
-    discountedDependents:0|1|2|3,
+    discountedDependents: 0 | 1 | 2 | 3;
 }
 export interface MyContextType {
-    currentFamilyMember: familyMember,
-    setCurrentFamilyMember: (newValue: familyMember) => void,
+    currentFamilyMember: familyMember;
+    setCurrentFamilyMember: (newValue: familyMember) => void;
 }
 
-export const CurrentMemberContext = createContext<MyContextType|undefined>(undefined);
+export const CurrentMemberContext = createContext<MyContextType | undefined>(
+    undefined
+);
 
 const HouseholdSalaryCalculator = () => {
     const defaultFamilyMember: familyMember = {
         id: 0,
         name: "Bendi",
         grossSalary: 250000,
-        netSalary:166250,
+        netSalary: 166250,
         under25: false,
         newlyWed: false,
         weddingDate: null,
         taxRelief: false,
         familyDiscount: false,
         dependents: 0,
-        discountedDependents:0
+        discountedDependents: 0,
     };
-    const [currentFamilyMember, setCurrentFamilyMember] = useState<familyMember>({
-        ...defaultFamilyMember,
-    });
+    const [currentFamilyMember, setCurrentFamilyMember] =
+        useState<familyMember>({
+            ...defaultFamilyMember,
+        });
     const [familyMembers, setFamilyMembers] = useState<familyMember[]>([
         defaultFamilyMember,
     ]);
     useEffect(() => {
-        const index = familyMembers.findIndex(member => member.id === currentFamilyMember.id);
+        const index = familyMembers.findIndex(
+            (member) => member.id === currentFamilyMember.id
+        );
         const updatedFamilyMembers = [...familyMembers];
         updatedFamilyMembers[index] = currentFamilyMember;
         setFamilyMembers(updatedFamilyMembers);
     }, [currentFamilyMember]);
-  const updateValue = (newValue: familyMember) => {
-    setCurrentFamilyMember(newValue);
-  };
-
-
-  const contextValue: MyContextType = {
-    currentFamilyMember:currentFamilyMember,
-    setCurrentFamilyMember: updateValue,
-  };
+    const updateValue = (newValue: familyMember) => {
+        setCurrentFamilyMember(newValue);
+    };
     const newMember = () => {
-        const newFamilyMember = { ...defaultFamilyMember, id: familyMembers.length };
-        setFamilyMembers([...familyMembers,newFamilyMember]);
+        const newFamilyMember = {
+            ...defaultFamilyMember,
+            id: familyMembers.length,
+        };
+        setFamilyMembers([...familyMembers, newFamilyMember]);
         setCurrentFamilyMember(newFamilyMember);
     };
-    const deleteCurrentFamilyMember = ()=>{
-        const index = familyMembers.findIndex(member => member.id === currentFamilyMember.id);
+    const deleteCurrentFamilyMember = () => {
+        const index = familyMembers.findIndex(
+            (member) => member.id === currentFamilyMember.id
+        );
         const updatedFamilyMembers = [...familyMembers];
         updatedFamilyMembers.splice(index, 1);
-        for (let i = index; i < familyMembers.length-1; i++) {
-            updatedFamilyMembers[i] = {...familyMembers[i+1],id:i};
-        }         
+        for (let i = index; i < familyMembers.length - 1; i++) {
+            updatedFamilyMembers[i] = { ...familyMembers[i + 1], id: i };
+        }
         setFamilyMembers(updatedFamilyMembers);
         setCurrentFamilyMember(updatedFamilyMembers[index]);
-    }
+    };
+    const contextValue: MyContextType = {
+        currentFamilyMember: currentFamilyMember,
+        setCurrentFamilyMember: updateValue,
+    };
     return (
-        <CurrentMemberContext.Provider value={contextValue} >
+        <CurrentMemberContext.Provider value={contextValue}>
             <header>
                 <FamilyMemberTabs
                     family={familyMembers}
@@ -83,7 +91,13 @@ const HouseholdSalaryCalculator = () => {
                 />
             </header>
             <main className="flex justify-center mt-2">
-                {familyMembers.length===0?<Skeleton className="w-1/2 rounded-lg bg-zinc-700 p-4 h-[592px]" />:<SalaryCalculator deleteCurrentFamilyMember={deleteCurrentFamilyMember} />}
+                {familyMembers.length === 0 ? (
+                    <Skeleton className="w-1/2 rounded-lg bg-zinc-700 p-4 h-[592px]" />
+                ) : (
+                    <SalaryCalculator
+                        deleteCurrentFamilyMember={deleteCurrentFamilyMember}
+                    />
+                )}
                 <HouseholdSummary family={familyMembers} />
             </main>
         </CurrentMemberContext.Provider>
